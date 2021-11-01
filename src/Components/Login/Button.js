bdad98fd446f6d42ca622399d23e28c972310d66
MehulPandya <mehulpandiya5@gmail.com>
2021-11-01 19:02:44 +0530

    first commit

--- src/Components/Login/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./LoginForm.css";
-
-const Button = () => {
-  return <input type="submit" className="fadeIn fourth" value="Log In" />;
-};
-
-export default Button;
